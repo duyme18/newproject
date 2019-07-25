f90251d401c319a209d1cae145658ac50a6bb9b1
HDD <duyme18@gmail.com>
2019-07-25 11:03:40 +0700

    add file

---
+++ JS.js
@@ -0,0 +1,8 @@
+let chieucao
+let chieurong
+chieucao =prompt('nhap chieu cao' )
+chieurong=prompt('nhap chieu rong')
+let width= parseInt(chieucao)
+let heigh=parseInt(chieurong)
+let s=chieucao*chieurong
+console.log('dien tich ='+s)
